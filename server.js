dee4b58c88c165263658450b2fffe0a531159ef2
林玉划 <linyuhua@linyuhuadeMacBook-Air.local>
2017-06-16 09:29:16 +0800

    init

---
+++ server.js
@@ -0,0 +1,16 @@
+var path = require('path');
+var express = require('express');
+
+var app = express();
+
+app.use('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+})
+
+app.listen(4000, function (err) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log('listening at http://localhost:4000');
+})
